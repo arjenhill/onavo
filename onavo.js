b176379e19fa2392dd7592bd18db024c0406c713
p_jiewwang_NPT <ahthw@hotmail.com>
2016-08-08 13:10:48 +0800

    新增览器检 $$TB.B , $$TB.IE()全版本测方法

--- onavo.js
+++ onavo.js
@@ -60,36 +60,43 @@ var $$, $$T, $$TB;
 			}
 			//cssload
 	};
-	//浏览器检测S
+	//$$TB专门用于 浏览器检测S
 	TB = (function(ua) {
-		var b = {
-			msie: /msie/.test(ua) && !/opera/.test(ua),
-			opera: /opera/.test(ua),
-			safari: /webkit/.test(ua) && !/chrome/.test(ua),
+		var B = {
+			mobile: (/iphone|nokia|sony|ericsson|mot|samsung|sgh|lg|philips|panasonic|alcatel|lenovo|cldc|midp|wap|android|iPod/i.test(ua)), //是否为移动终端
+			ios: !!ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+			android: ua.match(/android/i) == "android", //android终端或者uc浏览器
+			iPhone: ua.match(/iphone os/i) == "iphone os", //是否为iPhone或者QQHD浏览器
+			iPad: ua.match(/ipad/i) == "ipad", //是否iPad
+			webApp: ua.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+			chrome: /chrome/.test(ua),
 			firefox: /firefox/.test(ua),
-			chrome: /chrome/.test(ua)
+			safari: /webkit/.test(ua) && !/chrome/.test(ua),
+			opera: /opera/.test(ua)
 		};
-		var vMark = "";
-		for (var i in b) {
-			if (b[i]) {
-				vMark = "safari" == i ? "version" : i;
-				break;
-			}
+		//IE11的userAgent里是没有MSIE标志，加上IE版本差别多，所以单独$$TB.IE()方法
+		var IE = function() {
+			var sys = {};
+			var s;
+			(s = ua.match(/rv:([\d.]+)\) like gecko/)) ? sys.ie = s[1]:
+				(s = ua.match(/msie ([\d.]+)/)) ? sys.ie = s[1] : 0;
+			var _version = parseInt(sys.ie, 10);
+			if (sys.ie) {
+				return {
+					is: true,
+					version: _version
+				}
+			};
+			return false;
 		}
-		b.version = vMark && RegExp("(?:" + vMark + ")[\\/: ]([\\d.]+)").test(ua) ? RegExp.$1 : "0";
-
-		b.ie = b.msie;
-		b.ie6 = b.msie && parseInt(b.version, 10) == 6;
-		b.ie7 = b.msie && parseInt(b.version, 10) == 7;
-		b.ie8 = b.msie && parseInt(b.version, 10) == 8;
-		b.ie9 = b.msie && parseInt(b.version, 10) == 9;
-		b.ie10 = b.msie && parseInt(b.version, 10) == 10;
-		return b;
-		//IE11修改了 User-agent 字符串 使用时以> ie10 为准 
+		return {
+			B: B,
+			IE: IE
+		}
 	})(window.navigator.userAgent.toLowerCase());
 	// http://blog.csdn.net/zsj523/article/details/16802131
 	// 移掉ie6图像闪烁
-	if (TB.ie6) {
+	if (TB.IE().version == 6) {
 		try {
 			document.execCommand("BackgroundImageCache", false, true);
 		} catch (e) {}
